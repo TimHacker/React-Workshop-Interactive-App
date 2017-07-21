d9081c7bd5476b2fd442d44a934456c4e6ce4a75
Tim Hacker <tim.hacker@trueclarity.co.uk>
2017-07-21 16:42:13 +0100

    Added the ability to reset a counter

--- src/Counter.js
+++ src/Counter.js
@@ -17,12 +17,18 @@ class Counter extends React.Component {
     this.setState({ count: this.state.count - 1 });
   }
 
+  resetCount() {
+    this.setState({ count: 0 });
+  }
+
   render() {
     return (
       <div>
         <button onClick={() => this.decrement()}>-1</button>
         {this.state.count}
         <button onClick={() => this.increment()}>+1</button>
+        <br />
+        <button onClick={() => this.resetCount()}>Reset</button>
       </div>
     );
   }
